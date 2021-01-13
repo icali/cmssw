9ff4605629f6a3167add0f5668ea62dafe667a0c
Ivan <icali@cern.ch>
2021-01-13 19:28:43 +0100

    fixing compilation

--- RecoLocalTracker/SiStripDigiCompressor/plugins/SealModules.cc
+++
@@ -1,4 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "RecoLocalTracker/SiStripDigiCompressor/plugins/SiStripDigiCompressor.h"
-DEFINE_FWK_MODULE(SiStripDigiCompressor);
--- RecoLocalTracker/SiStripDigiCompressor/plugins/SiStripDigiCompressor.cc
+++ RecoLocalTracker/SiStripDigiCompressor/plugins/SiStripDigiCompressor.cc
@@ -2,10 +2,21 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Utilities/interface/transform.h"
 
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
+#include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
+#include "RecoLocalTracker/SiStripZeroSuppression/interface/SiStripRawProcessingFactory.h"
+#include "FWCore/Utilities/interface/transform.h"
+#include <memory>
+
 SiStripDigiCompressor::SiStripDigiCompressor(const edm::ParameterSet& conf)
-    : inputTags(conf.getParameter<std::vector<edm::InputTag>>("DigiProducersList")),
-      algorithm(StripClusterizerAlgorithmFactory::create(conf.getParameter<edm::ParameterSet>("Clusterizer"))) {
-  produces<edmNew::DetSetVector<SiStripCluster>>();
+    : inputTags(conf.getParameter<std::vector<edm::InputTag>>("DigiProducersList")){
+
+  produces<edmNew::DetSetVector<SiStripDigi>>();
   inputTokens = edm::vector_transform(
       inputTags, [this](edm::InputTag const& tag) { return consumes<edm::DetSetVector<SiStripDigi>>(tag); });
 }
@@ -17,9 +28,9 @@ void SiStripDigiCompressor::produce(edm::Event& event, const edm::EventSetup& es
   edm::Handle<edm::DetSetVector<SiStripDigi>> input;
 
   for (auto const& token : inputTokens) {
-    if (findInput(token, input, event))
+    if (findInput(token, input, event)){
       //algorithm->clusterize(*inputOld, *output);
-    else
+    } else
       edm::LogError("Input Not Found") << "[SiStripDigiCompressor::produce] ";  // << tag;
   }
 
@@ -33,3 +44,7 @@ inline bool SiStripDigiCompressor::findInput(const edm::EDGetTokenT<T>& tag, edm
   e.getByToken(tag, handle);
   return handle.isValid();
 }
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(SiStripDigiCompressor);
