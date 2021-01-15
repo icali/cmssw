08189ad5ad8dc3ffcfc7e7c632854544caaeb318
Ivan <icali@cern.ch>
2021-01-15 10:01:26 +0100

    reorganizing repo

--- RecoLocalTracker/SiStripDigiCompressor/python/SiStripDigiCompressor_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-siStripDigiCompressor = cms.EDProducer("SiStripDigiCompressor",
-                               DigiProducersList = cms.VInputTag(
-                                  cms.InputTag('siStripDigis','ZeroSuppressed')
-                               )
-)
-
-
