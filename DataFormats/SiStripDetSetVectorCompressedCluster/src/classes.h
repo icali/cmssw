672224dde5687ff139dbff5eb34714e5a8ece328
Ivan <icali@cern.ch>
2021-01-25 18:10:44 +0100

    adding Compressed DetSet

---
+++ DataFormats/SiStripDetSetVectorCompressedCluster/src/classes.h
@@ -0,0 +1,11 @@
+#ifndef SISTRIPDETSETVECTORCOMPRESSEDCLUSTER_CLASSES_H
+#define SISTRIPDETSETVECTORCOMPRESSEDCLUSTER_CLASSES_H
+
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/ContainerMask.h"
+
+#include "DataFormats/SiStripDetSetVectorCompressedCluster/interface/SiStripDetSetVectorCompressedCluster.h"
+
+#endif  // SISTRIPDETSETVECTORCOMPRESSEDCLUSTER_CLASSES_H
