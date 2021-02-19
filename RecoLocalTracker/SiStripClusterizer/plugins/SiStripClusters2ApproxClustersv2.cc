9b749191b345a6f40b458b549a209c3a1264649f
Ivan <icali@cern.ch>
2021-02-19 16:12:15 +0100

    remove comments

--- RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusters2ApproxClustersv2.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusters2ApproxClustersv2.cc
@@ -26,39 +26,24 @@ void SiStripClusters2ApproxClustersv2::produce(edm::Event& e, edm::EventSetup co
     edmNew::DetSetVector<SiStripApproximateClusterv2>::FastFiller ff = edmNew::DetSetVector<SiStripApproximateClusterv2>::FastFiller(*result, i->id());
 
     for( edmNew::DetSet<SiStripCluster>::const_iterator j = i->begin(); j!=i->end(); j++){
-      //std::cout << i->id() << " " << j->firstStrip() << " " << j->amplitudes().size() << " " << j->barycenter() << " " << minID << " " << maxFirst << std::endl;
 
       if(maxFirst<j->firstStrip()) maxFirst = j->firstStrip();
       if(minID>i->id()) minID = i->id();
 
-     // uint16_t firstStrip = j->firstStrip(); 
       uint8_t width = j->amplitudes().size();
       uint16_t barycenter = (uint16_t)j->barycenter();
 
       int charge = 0;
       for (unsigned k = 0; k < j->amplitudes().size(); k++) {
         charge += (int)j->amplitudes()[k];
-        //std::cout << (int)j->amplitudes()[k] << " " ;
       }
-      //std::cout << std::endl;
 
       SiStripApproximateClusterv2 approxCluster = SiStripApproximateClusterv2( (uint8_t)(charge/width), barycenter, width );
       ff.push_back(approxCluster);
-      //tempVec.push_back(approxCluster);      
     }
     
-    //edmNew::DetSet< SiStripApproximateClusterv2> tempDetSet = edmNew::DetSet< SiStripApproximateClusterv2>(i->id(), tempVec,0,0); 
-    //std::cout << tempDetSet.size() << std::endl;
 
-    //result->insert(tempDetSet);
   }
-
-  
-  //const edmNew::DetSetVector<SiStripCluster>&  clusters = *clusterCollection.product();
-
-
-
-  //std::cout << clusters.size() << std::endl;
 
   e.put(std::move(result));
 }
